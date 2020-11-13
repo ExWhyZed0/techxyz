34634c35fb7d81cdfad3dda0b8c3600f4e696c66
Stackbit <projects@stackbit.com>
2020-11-13 14:53:35 +0000

    initial commit by Stackbit

---
+++ content/_index.md
@@ -0,0 +1,8 @@
+---
+title: Home
+has_intro: true
+intro_content: "Just another tech fanatic here on DEV!\r\nWeb Developer | UI/UX Designer | Into Cyber Security & Digital Marketing"
+has_more_link: true
+more_link_text: Read more
+layout: home
+---
---
+++ stackbit-build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+set -v
+
+echo "stackbit-build.sh: start build"
+
+# fetch data from CMS through stackbit-pull
+npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5fae9dc1863f42001d56a32e
+
+# build site
+hugo
+
+echo "stackbit-build.sh: finished build"
